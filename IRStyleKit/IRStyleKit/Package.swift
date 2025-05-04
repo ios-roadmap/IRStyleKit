f2edbd1c73fb522038d67f3c62235fa138469202
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-04 11:49:27 +0300

    fix: Library error build

--- IRStyleKit/IRStyleKit/Package.swift
+++ IRStyleKit/IRStyleKit/Package.swift
@@ -15,11 +15,18 @@ let package = Package(
             name: "IRStyleKit",
             targets: ["IRStyleKit"]),
     ],
+    dependencies: [
+        .package(name: "IRCore", path: "../../../IRCore"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "IRStyleKit"),
+            name: "IRStyleKit",
+            dependencies: [
+                "IRCore"
+            ]
+        ),
         .testTarget(
             name: "IRStyleKitTests",
             dependencies: ["IRStyleKit"]
