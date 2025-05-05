05d7d790037b8c289f90016c4bb49c6295c13df9
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-05 08:37:03 +0300

    Build standard has changed.

---
+++ IRStyleKit/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IRStyleKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v18)
+    ],
+    products: [
+        .library(
+            name: "IRStyleKit",
+            targets: ["IRStyleKit"]),
+    ],
+    dependencies: [
+        .package(name: "IRCore", path: "../../IRCore/IRCore"),
+    ],
+    targets: [
+        .target(
+            name: "IRStyleKit",
+            dependencies: [
+                "IRCore"
+            ]
+        ),
+        .testTarget(
+            name: "IRStyleKitTests",
+            dependencies: ["IRStyleKit"]
+        ),
+    ]
+)
